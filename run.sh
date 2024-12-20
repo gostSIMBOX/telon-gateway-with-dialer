a31a0ea63f3ae2e36e698c11fe4aa2a7eea3e0d7
Anton Dodonov <anton.v.dodonov@gmail.com>
2024-12-20 10:19:29 +0700

    auto

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-yarn add https://github.com/telon-org/react-native-tele
-yarn add https://github.com/telon-org/react-native-replace-dialer
 yarn android
---
+++ yarn_add.sh
@@ -0,0 +1,9 @@
+#git config --global user.name "Your Name"
+#git config --global user.email "your@email.com"
+#git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+
+#yarn add git@github.com:telon-org/react-native-tele.git
+#yarn add git@github.com:telon-org/react-native-replace-dialer.git
+
+yarn add https://github.com/telon-org/react-native-tele
+yarn add https://github.com/telon-org/react-native-replace-dialer
